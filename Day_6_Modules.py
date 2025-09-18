8b73cd1a004c119f2f176390f513fd25ee57530d
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-18 17:03:59 -0400

    Added datetime module with two examples

--- Day_6_Modules.py
+++ Day_6_Modules.py
@@ -1,8 +1,16 @@
 import math
 import random
+import datetime
 
 print(math.sqrt(16))
 print(math.pi)
 
 print(random.randint(1,10))
 print(random.choice(["red", "blue", "green", "yellow"]))
+
+
+now = datetime.datetime.now()
+print(now)
+
+today = datetime.date.today()
+print(today)
