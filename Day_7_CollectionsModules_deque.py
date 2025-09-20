4706394d8a2bc1f4c2e16aaed778c42ea21ba6e4
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-19 22:37:48 -0400

    Add Counter from Collection Modules

--- Day_7_CollectionsModules_deque.py
+++ Day_7_CollectionsModules_deque.py
@@ -1,7 +1,15 @@
 from collections import deque
+from collections import Counter
+
 dq = deque([1, 2, 3])
 dq.appendleft(0)
 dq.append(4)
 dq.pop()
 dq.popleft()
 print(dq)
+
+
+words = ["apple", "banana", "apple", "orange", "banana", "apple"]
+count = Counter(words)
+print(count)
+print(count.most_common(1))
