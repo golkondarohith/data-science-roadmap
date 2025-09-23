86fe19b8a72ef74a499106475bfa8742bc34c01f
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-23 00:00:07 -0400

    Add show_history() function

--- Mini_Project_2.py
+++ Mini_Project_2.py
@@ -1,1 +1,21 @@
 #Calculator with History
+
+# with open("student.txt", "r") as f:
+#     for line in f:
+#         name, grade = line.strip().split(",")
+#         loaded_students.append(Student(name, grade))
+
+
+history_file = "calc_history.txt"
+
+def show_history():
+    with open(history_file, "r") as f:
+        history = f.readlines()
+        if len(history) == 0:
+            print("No history available.")
+        else:
+            print("Calculation History:")
+            for entry in history:
+                print(entry.strip())
+        
+
