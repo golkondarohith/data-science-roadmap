06695ef78ee568556fe3bc04efb852cac3efdd69
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-24 22:57:24 -0400

    Add Grade method

--- MIni_Project_3_OOPs.py
+++ MIni_Project_3_OOPs.py
@@ -1,8 +1,21 @@
+from datetime import datetime
+
 class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
 
+#Allocating grade
+    def grade(self):
+        if self.marks >= 90:
+            return "A"
+        elif self.marks >= 75:
+            return "B"
+        elif self.marks >= 50:
+            return "C"
+        else:
+            return "F"
+    
 # Function to calculate average marks
 def average_marks(students):
     return sum(student.marks for student in students) / len(students) if students else 0
@@ -30,7 +43,15 @@ n = int(input("Enter number of students: "))
 
 for i in range(n):
     student_name = input(f"Enter the name of student {i+1}: ")
-    marks = int(input(f"Enter the marks for student {i+1}: "))
+
+    # keep asking until valid marks
+    while True:
+        try:
+            marks = int(input(f"Enter the marks for student {i+1}: "))
+            break
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+    
     students.append(Student(student_name, marks))
 
 # Print student scores
@@ -38,9 +59,10 @@ for student in students:
     print(f"Student {student.name} scored {student.marks}")
 
 # Write student scores to file
-with open("student.txt", "w") as f:
+with open("student.txt", "a") as f:
+    f.write("Report generated on: " + str(datetime.now()) + "\n\n")
     for student in students:
-        f.write(f"Student {student.name} scored {student.marks}\n")
+        f.write(f"Student {student.name} scored {student.marks} (Grade {student.grade()})\n")
 
 # Calculate statistics
 avg = average_marks(students)
@@ -51,3 +73,8 @@ low_marks, bottom_students = lowest_marks(students)
 print(f"\nClass Average: {avg:.2f}")
 print(f"Highest Marks: {high_marks} by {', '.join(top_students)}")
 print(f"Lowest Marks: {low_marks} by {', '.join(bottom_students)}")
+print("\nFinal Student Grades:")
+for student in students:
+    print(f"Student {student.name} scored {student.marks} → Grade {student.grade()}")
+
+
