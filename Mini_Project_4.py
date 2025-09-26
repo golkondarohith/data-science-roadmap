596ab8d9b553bb58f45a74ddaa9194bd1b447220
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-25 23:34:35 -0400

    Add save_records method

--- Mini_Project_4.py
+++ Mini_Project_4.py
@@ -1,36 +1,45 @@
 #Library Managemnet System
 class Library():
-    def __init__(self, book_title, book_author, available_copies):
+    def __init__(self, book_title, book_author, available_copies = 1):
         self.book_title = book_title
         self.book_author = book_author
         self.available_copies = available_copies
 
-#Adding a book
-def add_book():
-        available_copies = 0
-        book_title = input("Enter the book title: ")
-        book_author = input("Enter the book author name: ")
-        for book, author in books:
-            if book == "book_title" and author == "book_author":
-                available_copies += 1
-        books.append(Library(book_title, book_author, available_copies))
-        print("Book added to library successfully....")
 
-
-#Main program
+#List to store books
 books = []
 
 
-#Writing into the file
-with open("library_records.txt", "a") as f:
-     for book in books:
-          f.write(f"Book {book.book_title} wwritten by {book.book_author} and available copies {book.available_copies}")
+#Function to add a book
+def add_book():
+        book_title = input("Enter the book title: ")
+        book_author = input("Enter the book author name: ")
+
+        #check if book already exist
+        for book in books:
+            if book.book_title == book_title and book.author == book_author:
+                book.available_copies += 1
+                print("Another copy added successfully!")
+                return
+        #if book doesn't exists, add new
+        books.append(Library(book_title, book_author, 1))
+        print("Book added to library successfully....")
+
+#Function to save records into file
+def save_records():
+    with open("library_records.txt", "a") as f:
+         for book in books:
+            f.write(
+                f"Book '{book.book_title}' written by {book.book_author}, "
+                f"Available copies: {book.available_copies}\n"
+            )
 
 
-
-
+#Main program
 add_book()
+save_records()
 
-#Printing the books
-for book in books:
-    print(f"Book {book.book_title} written by {book.book_author}and available copies {book.available_copies}")
+
+
+
+
