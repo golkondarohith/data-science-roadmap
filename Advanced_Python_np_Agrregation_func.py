0582adcaa2575d363f70223c81c08f58d441700f
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-30 23:40:27 -0400

    Add Aggregate functions

---
+++ Advanced_Python_np_Agrregation_func.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+#Sum of all elements
+print(np.sum(arr))
+print("\n")
+
+#Mean
+print(np.mean(arr))
+print("\n")
+
+#Highest element
+print(np.max(arr))
+print("\n")
+
+#Lowest element
+print(np.min(arr))
+print("\n")
+
+
+#Standard deviation
+print(np.std(arr))
+print("\n")
+
+#variance
+print(np.var(arr))
+print("\n")
+
