c2b7cae8667d90d2204b509d5773eef50d539cd9
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-30 23:29:28 -0400

    Practice Array Maths Properties

---
+++ Advanced_Python_np_arrays_maths_4.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+arr = np.array([10, 20, 30])
+print(arr)
+print("\n")
+
+print(arr+5)
+print("\n")
+
+print(arr-2)
+print("\n")
+
+print(arr/3)
+print("\n")
+
+print(arr*2)
+print("\n")
+
+print(arr**2)
+print("\n")
+
