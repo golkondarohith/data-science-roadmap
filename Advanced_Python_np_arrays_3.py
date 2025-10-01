52fb14a79703babe65ed2d51a5e3db49d9c0394e
Rohith Golkonda <rohithpatel105@gmail.com>
2025-09-30 23:21:46 -0400

    Practice Array properties

---
+++ Advanced_Python_np_arrays_3.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+#Finding the shape of the array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr_2d.shape)
+print("\n")
+
+
+#Finding the size of the array
+print(arr_2d.size)
+print("\n")
+
+
+#Finding the dimensions of the array
+print(arr_2d.ndim)
+print("\n")
+
+
+#Finding the data type of the elements
+print(arr_2d.dtype)
+print("\n")
+
+
+#Changing the datatype
+print(arr_2d.astype(float))
+print("\n")
