3565b6848697cddf8e37b27b3b08e8a2da2d74b7
Rohith Golkonda <rohithpatel105@gmail.com>
2025-10-02 22:31:19 -0400

    Practice random numbers generation

---
+++ numpy_pracatice_1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+random_integers = np.random.randint(1, 101, size=20)
+print(random_integers)
+
+random_integers1 = np.random.choice(np.arange(1, 101), size=20, replace=False) 
+print(random_integers1)
