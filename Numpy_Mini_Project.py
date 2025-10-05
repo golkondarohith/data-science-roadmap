a208bc910be43123d1b2a562aec31ba7b7789fd1
Rohith Golkonda <rohithpatel105@gmail.com>
2025-10-05 16:38:14 -0400

    practice numpys with real world problem

--- Numpy_Mini_Project.py
+++ Numpy_Mini_Project.py
@@ -3,3 +3,29 @@ import numpy as np
 data = np.genfromtxt('students.csv', delimiter = ",", skip_header = 1)
 
 print(data)
+
+marks = data[:, 3:]
+
+# print(marks)
+print("Mean scores:\n ", np.mean(marks, axis = 0))
+print("Max scores:\n ", np.max(marks, axis=0))
+print("Min scores:\n", np.min(marks, axis=0))
+
+high_sci = data[data[:, 4] > 85]
+print(high_sci)
+
+totals = np.sum(marks, axis=1)
+averages = np.mean(marks, axis=1)
+
+print("Totals:", totals)
+print("Avengers:", averages)
+
+data[2, 3] = np.nan
+
+col_means = np.nanmean(data[:, 3:], axis=0)
+inds = np.where(np.isnan(data))
+data[inds] = np.take(col_means, inds[1]-3)
+
+
+print(data)
+
