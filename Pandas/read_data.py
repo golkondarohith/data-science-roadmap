e895441ceb695819a8f837532cf1b3658a5999ad
Rohith Golkonda <rohithpatel105@gmail.com>
2025-10-07 21:32:50 -0400

    Add comments to file

--- Pandas/read_data.py
+++ Pandas/read_data.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 df = pd.read_json("sample_Data.json")
 
+#Retrieving the first 10rows
 print("Display first 10 rows: \n")
 print(df.head(10))
 
+#Retrieving the last 10 rows 
 print("Display last 10 rows: \n")
 print(df.tail(10))
