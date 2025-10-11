ee80212e86c5f606f30b2be7068e0cdd17d29295
Rohith Golkonda <rohithpatel105@gmail.com>
2025-10-11 11:33:26 -0400

    Removing a single row from the dataset(Performance_score)

---
+++ Pandas/removing_rows.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+
+data = {
+    "Name": ['Ram', 'Shyam', 'Ghanshyam', 'Dhanshyam', 'Aditi', 'Jagdish', 'Raj', 'Simran'],
+    "Age": [28, 34, 22, 30, 29, 40, 25, 32],
+    "Salary": [50000, 60000, 45000, 52000, 49000, 70000, 48000, 58000],
+    "Performance_score": [85, 90, 78, 92, 88, 95, 80, 89]
+}
+
+
+df = pd.DataFrame(data)
+print("Display Original data: \n", df)
+
+#Removing a single row from the dataset(Performance_score)
+#syntax - df.drop(columns = ["Column_Name"], inplace = True)
+
+df.drop(columns = ["Performance_score"], inplace = True)
+print(df)
