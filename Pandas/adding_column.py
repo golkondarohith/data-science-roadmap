be1dd624af39344f661fcbb8f5e148cf46f0b044
Rohith Golkonda <rohithpatel105@gmail.com>
2025-10-11 11:12:23 -0400

    Adding a bonus column to the extsting data

---
+++ Pandas/adding_column.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+data = {
+    "Name": ['Ram', 'Shyam', 'Ghanshyam', 'Dhanshyam', 'Aditi', 'Jagdish', 'Raj', 'Simran'],
+    "Age": [28, 34, 22, 30, 29, 40, 25, 32],
+    "Salary": [50000, 60000, 45000, 52000, 49000, 70000, 48000, 58000],
+    "Performance_score": [85, 90, 78, 92, 88, 95, 80, 89]
+}
+
+
+df = pd.DataFrame(data)
+print("Display Original data: \n", df)
+
+#Adding a bonus column to the extsting data
+df["Bonus"] = df["Salary"] * 0.1
+print("Displaying the Modified data: \n", df)
