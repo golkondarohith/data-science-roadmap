8064a59bfa84abb14a7f684a5dfae15e67345f10
Rohith Golkonda <rohithpatel105@gmail.com>
2025-11-14 01:16:57 -0500

    List comprehensions

--- list_comprehension.py
+++ list_comprehension.py
@@ -10,3 +10,16 @@ only_even = [i if i%2 == 0 else 0 for i in nums]
 print(only_even)
 
 
+# Nested loop comprehensions
+a = [1, 2]
+b = [11, 22]
+
+pairs = [(i, j) for i in a for j in b]
+print(pairs)
+
+#Enumerates
+enum = [4, 5, 6, 7, 8]
+print("before enumeration", enum)
+enum = [(i, v) for i, v in enumerate(enum)]
+print(enum)
+
