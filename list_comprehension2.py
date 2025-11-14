7915d1a65ea4b17517e23228b99ee2196458849c
Rohith Golkonda <rohithpatel105@gmail.com>
2025-11-14 01:39:01 -0500

    List Comprehensions(Vowels in String)

--- list_comprehension2.py
+++ list_comprehension2.py
@@ -2,3 +2,9 @@
 cubes = [i*i*i for i in range(1,11)]
 print(cubes)
 
+#Extract all vowels from a string
+string = "My name is Alice"
+vowels = "aeiouAEIOU"
+
+vowels_string = [c for c in string if c in vowels]
+print(vowels_string)
