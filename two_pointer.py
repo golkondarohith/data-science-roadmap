09a386b62a5d707ae536385ffb416412453466ba
Rohith Golkonda <rohithpatel105@gmail.com>
2025-11-16 13:09:35 -0500

    Slinding Window( Max sub Array)

--- two_pointer.py
+++ two_pointer.py
@@ -14,3 +14,16 @@ def targetSum(arr):
             left += 1
 
 print(targetSum([1, 2, 4, 7, 11, 15]))
+
+
+def maxSubArr(arr, k):
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+
+    for i in range(k, len(arr)):
+        window_sum = window_sum - arr[i - k] + arr[i]
+        max_sum = max(window_sum, max_sum)
+    return max_sum
+
+print(maxSubArr([2, 1, 5, 1, 3, 2], 3))
+    
