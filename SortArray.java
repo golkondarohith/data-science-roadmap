ff466af2aa17ce0608cf27f5939ba306ac0eda17
Rohith Golkonda <rohithpatel105@gmail.com>
2025-11-21 00:11:52 -0500

    Java Sorting

---
+++ SortArray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class SortArray {
+    public static void main(String[] args){
+        int arr[] = {5, 3, 1, 4, 2};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
